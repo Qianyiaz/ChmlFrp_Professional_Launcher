e9adfd4171d4453b8dc3fa3cd5c38e831e7d2040
Qusay Diaz <Qusay_Diaz.github@outlook.com>
2025-03-14 20:10:52 +0800

    使用.NET.Sdk😁

---
+++ .cpl/Properties/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace ChmlFrp_Professional_Launcher
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        // 入口
+        static App()
+        {
+            new MainClass.Paths();
+        }
+    }
+}
